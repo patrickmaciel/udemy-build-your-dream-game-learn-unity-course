eaab4a948352677a99abee7e76e860488b284634
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-09-15 23:01:04 -0300

    87. 3D physics - colliders

---
+++ 3D Game Physics/Assets/HelloColliders.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloColliders : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision c)
+    {
+        Debug.Log("Enter");
+    }
+
+    void OnCollisionStay(Collision c)
+    {
+        Debug.Log("Stay");
+    }
+
+    void OnCollisionExit(Collision c)
+    {
+        Debug.Log("Exit");
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        Debug.Log("Triggered");
+    }
+}
