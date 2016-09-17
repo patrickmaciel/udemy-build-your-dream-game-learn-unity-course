f35f4110328ff9a4d295f57f9f11ba23b0bb7c76
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-09-17 10:21:42 -0300

    94. 3D Physics - Capturing Player Input

---
+++ 3D Game Physics/Assets/HelloInput.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        this.gameObject.transform.Translate(horizontal, 0, vertical);
+    }
+}
