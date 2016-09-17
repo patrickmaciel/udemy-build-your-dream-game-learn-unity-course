0022a5fdf1734b02bedc59ce896639193ac0c289
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-09-17 14:51:57 -0300

    100. 3d balance ball game - adding physics & moving camera

---
+++ 3D Balance Ball/Assets/Code/BallController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+    public float force = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Debug.Log("horizontal = " + horizontal);
+        Debug.Log("vertical = " + vertical);
+
+        if (vertical > 0)
+        {
+            this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * force);
+        }
+
+        if (vertical < 0)
+        {
+            this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.back * force);
+        }
+
+        if (horizontal > 0)
+        {
+            this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.right * force);
+        }
+
+        if (horizontal < 0)
+        {
+            this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.left * force);
+        }
+    }
+}
---
+++ 3D Balance Ball/Assets/Code/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject ball = GameObject.Find("Ball");
+        Vector3 ballPosition = ball.transform.position;
+        this.gameObject.transform.position = new Vector3(ballPosition.x, ballPosition.y + 3f, ballPosition.z - 3f);	
+	}
+}
