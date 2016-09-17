26ed7c9836f0b0e42ad3a80912b574608140d2f0
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-09-17 10:12:27 -0300

    92. 3D Physics - Raycasting

---
+++ 3D Game Physics/Assets/HelloRaycasting.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloRaycasting : MonoBehaviour {
+
+    bool doorClosed = true;
+    public GameObject sphere;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 backward = new Vector3(0, 0, -1);
+        Vector3 position = sphere.transform.position;
+        Vector3 origin = this.gameObject.transform.position;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        Vector3 distant = transform.TransformDirection(Vector3.forward);
+
+        //if (Physics.Raycast(backward, origin, 1f) && doorClosed)
+        if (Physics.Raycast(position, fwd, 1f) && doorClosed)
+        {
+            doorClosed = false;
+            GameObject.Find("DoorL").transform.Translate(new Vector3(-.8f, 0, 0));
+            GameObject.Find("DoorR").transform.Translate(new Vector3(.8f, 0, 0));
+        }
+        else if (Physics.Raycast(origin, backward, 3f) && !doorClosed)
+        {
+            doorClosed = true;
+            GameObject.Find("DoorL").transform.Translate(new Vector3(0, 0, 0));
+            GameObject.Find("DoorR").transform.Translate(new Vector3(0, 0, 0));
+        }
+
+    }
+}
