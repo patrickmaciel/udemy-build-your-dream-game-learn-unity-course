25953adebd9b9f52d9e7f95aede8ef5a5ad6e26b
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-09-18 19:08:46 -0300

    122. 3d balance ball game - adding prefab challenges

---
+++ 3D Balance Ball/Assets/Code/DeathChecker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DeathChecker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.gameObject.transform.position.y < -5)
+        {
+            SceneManager.LoadScene(0);
+        }	
+	}
+}
---
+++ 3D Balance Ball/Assets/Code/EnemyCreator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCreator : MonoBehaviour {
+
+    public int count = 0;
+    public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+	    for (int i = 0; i < this.count; i++)
+        {
+            GameObject newGameObject = (GameObject)GameObject.Instantiate(prefab);
+            newGameObject.transform.position = new Vector3(0, 1, 12);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
