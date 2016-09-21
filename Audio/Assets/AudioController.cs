5210e29d78569187425d20fcf5ef29f8688469ba
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-09-20 22:57:52 -0300

    148. playing sounds - using snapshots to create underwater effects

---
+++ Audio/Assets/AudioController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+public class AudioController : MonoBehaviour
+{
+
+    public AudioMixerSnapshot normal;
+    public AudioMixerSnapshot underwater;
+
+    private bool isNormal = true;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isNormal)
+            {
+                underwater.TransitionTo(.2f);
+                isNormal = false;
+            }
+            else
+            {
+                normal.TransitionTo(.2f);
+                isNormal = true;
+            }
+        }
+    }
+}
