d8cc60211b5c8c158e6d7e7e4cf95bf9069094c0
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-09-24 18:07:48 -0300

    164 Mechanim Animations - Animation Curves

--- Animations/Assets/TeddyAnimationManager.cs
+++ Animations/Assets/TeddyAnimationManager.cs
@@ -12,5 +12,11 @@ public class TeddyAnimationManager : MonoBehaviour {
 	void Update () {
         //Animator animator = this.GetComponent<Animator>();
         //animator.SetTrigger("No");
+
+        Animator animator = this.GetComponent<Animator>();
+        if (animator.GetFloat("Walking") >= .04f && animator.GetFloat("Walking") <= .06f)
+        {
+            Debug.Log("Kick the ball");
+        }
 	}
 }
