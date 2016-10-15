3c508f2999fff87a570f571fcb3e69671d2309e3
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-10-15 15:26:58 -0300

    214. Platform Run Game - Collecting Coins

--- Platform Run/Assets/Code/CharController.cs
+++ Platform Run/Assets/Code/CharController.cs
@@ -54,4 +54,14 @@ public class CharController : MonoBehaviour {
 		grounded = true;
 		this.GetComponent<Animator>().speed = 1;
 	}
+
+	/// <summary>
+	/// Sent when another object enters a trigger collider attached to this
+	/// object (2D physics only).
+	/// </summary>
+	/// <param name="other">The other Collider2D involved in this collision.</param>
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Destroy(other.gameObject);
+	}
 }
