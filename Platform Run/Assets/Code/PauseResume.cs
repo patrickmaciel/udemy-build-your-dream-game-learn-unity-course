13e05e58dcc1cf2db5243e9448ff87ff682dd0c4
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-10-15 16:01:45 -0300

    235. Platform Run Game - Adding Pause/Resume Button

---
+++ Platform Run/Assets/Code/PauseResume.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PauseResume : MonoBehaviour {
+	public Sprite pauseSprite;
+	public Sprite resumeSprite;
+	bool paused = false;
+
+	public void PauseOrResume()
+	{
+		if (paused) {
+			Time.timeScale = 1;
+			this.GetComponent<Image>().sprite = pauseSprite;
+			GameObject.Find("Main Camera").GetComponent<AudioSource>().Play();
+			paused = false;
+		} else {
+			Time.timeScale = 0;
+			this.GetComponent<Image>().sprite = resumeSprite;
+			GameObject.Find("Main Camera").GetComponent<AudioSource>().Pause();
+			paused = true;
+		}
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
