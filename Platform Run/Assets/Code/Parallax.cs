c6b76138339b1f3643bfcd6aec5830d5587a3b45
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-10-14 21:29:29 -0300

    197. Platform Run - Building Our Game Scene

---
+++ Platform Run/Assets/Code/Parallax.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+    public float scrollSpeed;
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        float x = Mathf.Repeat(Time.time * scrollSpeed, 1);
+        this.gameObject.GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", new Vector2(x, 0f));
+    }
+}
