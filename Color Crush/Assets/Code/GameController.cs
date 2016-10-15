eb70b8e31ab2515e8cf9b3c929caff02e407ee7b
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-10-15 19:04:10 -0300

    240. Creating The Crush

--- Color Crush/Assets/Code/GameController.cs
+++ Color Crush/Assets/Code/GameController.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class GameController : MonoBehaviour {
 	int width = 10;
 	int height = 10;
@@ -55,7 +55,120 @@ public class GameController : MonoBehaviour {
 
 			y-= blockheight;
 			x = startX;
+		}
+
+		while (Crush() > 0) {
+
 		} 
+	}
+
+	public int Crush(bool check = false) {
+		int numberOfCrushes = 0;
+
+		// move right to check 3 or more similar to crush them
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < width; j++) {
+				if (grid[i, j] != null) {
+					int counter = 1;
+					BlockController.BlockType type = grid[i, j].GetComponent<BlockController>().type;
+
+					for (int k = j+1; k < width; k++) {
+						if (grid[i, k] == null) {
+							break;
+						}
+
+						BlockController.BlockType currentType = grid[i, k].GetComponent<BlockController>().type;
+
+						if (currentType == type) {
+							counter++;
+						} else {
+							break;
+						}
+					}
+
+					if (counter >= 3) {
+						numberOfCrushes++;
+
+						if (check) {
+							continue;
+						}
+
+						// Crush
+						for (int k = j; k < j; k++) {
+							Destroy(grid[i, k]);
+							grid[i, k] = null;
+						}
+
+						// shift cells down in the grid
+						for (int t = j; t < j + counter; t++) {
+							for (int r = i; r > 0; r--) {
+								grid[r, t] = grid[r-1, t];
+							}
+
+							grid[0, t] = null;
+						}
+
+						// this.DebugGrid();
+						score += counter;
+					}
+				}
+			}
+		}
+
+		// move vertically
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				if (grid[i, j] != null) {
+					int counter = 1;
+					BlockController.BlockType type = grid[i, j].GetComponent<BlockController>().type;
+
+					for (int k = i+1; k < height; k++) {
+						if (grid[k, j] == null) {
+							break;
+						}
+
+						BlockController.BlockType currentType = grid[k, j].GetComponent<BlockController>().type;
+						if (currentType == type) {
+							counter++;
+						} else {
+							break;
+						}
+					}
+
+					if (counter >= 3) {
+						numberOfCrushes++;
+
+						// if check is true, this means that we don't want to crush, we only wanto to check if there is 
+						if (check) {
+							continue;
+						}
+
+						// Crush
+						for (int k = i; k < i + counter; k++) {
+							Destroy(grid[k, j]);
+							grid[k, j] = null;
+						}
+
+						// shift cells down in the grid
+						for (int r = i -1; r >= 0; r--) {
+							grid[r + counter, j] = grid[r, j];
+							grid[r, j] = null;
+						}
+
+						// this.DebugGrid();
+						score += counter;
+					}
+				}
+			}
+		}
+
+		UpdateScore();
+
+		return numberOfCrushes;
+	}
+
+	void UpdateScore() {
+		scoreObject.GetComponent<Text>().text = "Score: " + score;
 	}
 	
 	// Update is called once per frame
